247ab457a82c53034b95c50ebb439df5456abc16
unknown <piyush88>
2026-01-21 20:07:09 +0530

    edit some css by me

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,56 +1,62 @@
 import { Link } from "react-router-dom";
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <>
-       <footer className="footer-area">
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-4">
-                    <div className="footer-text">
-                        <span>EpicConverter</span>
-                        <p>ConvertBhai is your go-to solution for all file
-                            conversion needs. Simple, fast, and secure.</p>
-                    </div>
-                    </div>
-
-                    <div className="col-lg-8">
-                        <div className="footer-list">
-                            <div className="footer-listfirst">
-                                <span>Product</span>
-                                <ul>
-                                    <li><Link to={"/"}>Tools</Link></li>
-                                     <li><Link to={"/"}>API</Link></li>
-                                      <li><Link to={"/"}>Pricing</Link></li>
-                                     
-                                </ul>
+            <footer className="footer-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="footer-text">
+                                <span>EpicConverter</span>
+                                <p>ConvertBhai is your go-to solution for all file
+                                    conversion needs. Simple, fast, and secure.</p>
                             </div>
+                        </div>
 
-                              <div className="footer-listfirst">
-                                <span>Company</span>
-                                <ul>
-                                    <li><Link to={"/"}>About Us</Link></li>
-                                     <li><Link to={"/"}>Blog</Link></li>
-                                      <li><Link to={"/"}>Contact</Link></li>
-                                     
-                                </ul>
+                        <div className="col-lg-8">
+                            <div className="footer-list">
+                                <div className="footer-listfirst">
+                                    <span>Product</span>
+                                    <ul>
+                                        <li><Link to={"/"}>Tools</Link></li>
+                                        <li><Link to={"/"}>API</Link></li>
+                                        <li><Link to={"/"}>Pricing</Link></li>
+
+                                    </ul>
+                                </div>
+
+                                <div className="footer-listfirst">
+                                    <span>Company</span>
+                                    <ul>
+                                        <li><Link to={"/"}>About Us</Link></li>
+                                        <li><Link to={"/"}>Blog</Link></li>
+                                        <li><Link to={"/"}>Contact</Link></li>
+
+                                    </ul>
+                                </div>
+
+                                <div className="footer-listfirst">
+                                    <span>Legal</span>
+                                    <ul>
+                                        <li><Link to={"/"}>Privacy Policy</Link></li>
+                                        <li><Link to={"/"}>Terms of Service</Link></li>
+                                        <li><Link to={"/"}>Cookie Policy</Link></li>
+
+                                    </ul>
+                                </div>
                             </div>
+                        </div>
 
-                              <div className="footer-listfirst">
-                                <span>Legal</span>
-                                <ul>
-                                    <li><Link to={"/"}>Privacy Policy</Link></li>
-                                     <li><Link to={"/"}>Terms of Service</Link></li>
-                                      <li><Link to={"/"}>Cookie Policy</Link></li>
-                                     
-                                </ul>
+                        <div className="col-lg-12">
+                            <div className="footer-copyright">
+                                
                             </div>
                         </div>
                     </div>
-            </div>
-        </div>
+                </div>
 
-       </footer>
+            </footer>
         </>
     )
 }
